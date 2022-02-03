edd4fdcbee33929ede4de355580f4a17db8d5a57
qkessler <qkessler@gmail.com>
2022-02-03 12:51:16 +0100

    feat: add cli args and its logic
    What we do is that we take the value that is passed in the command line
    args and if present in the config file we update it. If not present, it
    should be the keyboard to be used, so should be put at the beginning of
    the hashmap. I'm still not sure how to do so.

--- src/list_devices.rs
+++ src/list_devices.rs
@@ -2,15 +2,10 @@ extern crate simple_error;
 extern crate udev;
 
 use std::collections::HashMap;
-use std::error::Error;
-use std::process::{Command, Output};
 
-use simple_error::bail;
-use udev::Device;
 use udev::Enumerator;
 
 const ID_SERIAL: &str = "ID_SERIAL";
-type BoxResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn find_for_serial_ids(
     enumerator: &mut Enumerator,
@@ -26,20 +21,4 @@ pub fn find_for_serial_ids(
                 .collect::<Vec<String>>()
         })
         .find(|keyboard_serial_id| keyboards.contains_key(keyboard_serial_id.as_str()))
-}
-
-pub fn find_by_serial_id(enumerator: &mut Enumerator, id_serial: &str) -> Option<Device> {
-    enumerator.scan_devices().unwrap().find(|d| {
-        d.properties().any(|p| {
-            p.name().to_str().unwrap() == ID_SERIAL && p.value().to_str().unwrap() == id_serial
-        })
-    })
-}
-
-// TODO: should probably take arguments or read from the config file.
-pub fn set_layout(program: &str, args: &[&str]) -> BoxResult<Output> {
-    return match Command::new(program).args(args).output() {
-        Ok(output) => Ok(output),
-        Err(error) => bail!("set_layout error: {:?}", error),
-    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 mod list_devices;
 
 use common_macros::hash_map;
+use clap::Parser;
 use home::home_dir;
 use list_devices::find_for_serial_ids;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fs::File, io::BufReader, process::Command};
 use udev::Enumerator;
 
-use list_devices::set_layout;
+const CONFIG_FILE_PATH: &str = "/.config/layout_switcher/config.json";
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct LayoutSwitcherConfig {
     keyboards: HashMap<String, Vec<String>>,
+}
+
+/// Program to change the keyboard layout depending on the
+/// usb keyboard that is connected, with either the command line
+/// arguments or the config file under the
+/// ".config/layout_switcher/config.json" file.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Keyboard serial id.
+    #[clap(short, long)]
+    keyboard: Option<String>,
+
+    /// Commands to run if the keyboard is connected, in JSON format.
+    #[clap(short, long)]
+    commands: Option<String>,
 }
 
 fn run_commands(commands: &[String]) {
@@ -29,17 +46,34 @@ fn run_commands(commands: &[String]) {
     });
 }
 
-fn main() {
-    let config_file_path = "/.config/layout_switcher/config.json";
+fn parse_args() -> Option<(String, Vec<String>)> {
+    let args = Args::parse();
+
+    match (args.keyboard, args.commands) {
+        (Some(keyboard_id), Some(commands)) => {
+            Some((keyboard_id, serde_json::from_str(&commands).unwrap()))
+        }
+        _ => None,
+    }
+}
+
+fn get_config_from_file() -> LayoutSwitcherConfig {
     let config_file_str = format!(
         "{}{}",
         home_dir().unwrap().to_str().unwrap(),
-        config_file_path
+        CONFIG_FILE_PATH
     );
 
     let config_file = File::open(config_file_str).unwrap();
     let reader = BufReader::new(config_file);
-    let layout_config: LayoutSwitcherConfig = serde_json::from_reader(reader).unwrap();
+
+    serde_json::from_reader(reader).unwrap()
+}
+
+fn main() {
+    let keyboard_commands_args = parse_args();
+
+    let layout_config = get_config_with_args(keyboard_commands_args.as_ref());
 
     let mut enumerator = Enumerator::new().unwrap();
     let keyboard_id = find_for_serial_ids(&mut enumerator, &layout_config.keyboards);
@@ -65,8 +99,31 @@ fn main() {
         3. Else, do nothing.
     }
     */
+
+    for device in enumerator.scan_devices().unwrap() {
+        for property in device.properties() {
+            if property.name().to_str().unwrap() == "ID_SERIAL" {
+                println!("    - {:?} {:?}", property.name(), property.value());
+            }
+        }
+    }
     // let output = set_layout("xkblayout-state", &["print", "%v"]).unwrap();
     // println!("{:?}", from_utf8(&output.stdout).unwrap());
+}
+
+fn get_config_with_args(
+    keyboard_commands_args: Option<&(String, Vec<String>)>,
+) -> LayoutSwitcherConfig {
+    let mut layout_config = get_config_from_file();
+    match keyboard_commands_args {
+        Some((keyboard, commands)) => {
+            if layout_config.keyboards.contains_key(keyboard) {
+                layout_config.keyboards.insert(keyboard.clone(), commands.clone());
+            }
+            layout_config
+        },
+       None => layout_config
+    }
 }
 
 #[cfg(test)]
