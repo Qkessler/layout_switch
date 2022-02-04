cd17e1821e64f0b07938674999a5e0b4ea457e42
qkessler <qkessler@gmail.com>
2022-02-04 20:51:18 +0100

    feat: add updated find with libusb function

--- src/list_devices.rs
+++ src/list_devices.rs
@@ -24,18 +24,23 @@ pub fn find_for_serial_ids(
         .find(|keyboard_serial_id| keyboards.contains_key(keyboard_serial_id.as_str()))
 }
 
-pub fn find_with_libusb() {
+pub fn find_with_libusb(keyboards: &HashMap<String, Vec<String>>) -> Option<String> {
     let context = libusb::Context::new().unwrap();
 
     for device in context.devices().unwrap().iter() {
-        let device_desc = device.device_descriptor().unwrap();
-        // println!("{}", device_desc.serial_number_string_index().unwrap());
+        let device_desc = match device.device_descriptor() {
+            Ok(d) => d,
+            Err(_) => continue,
+        };
 
-        println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
-            device.bus_number(),
-            device.address(),
+        let device_id = format!(
+            "{:04x}:{:04x}",
             device_desc.vendor_id(),
-            device_desc.product_id());
+            device_desc.product_id()
+        );
+        if keyboards.contains_key(device_id.as_str()) {
+            return Some(device_id);
+        }
     }
+    return None;
 }
-
